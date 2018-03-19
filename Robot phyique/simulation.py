8834c683dbb31eccb7b65a73d6c9f0bb809a86f2
FabienManson <fabien.manson@free.fr>
2018-03-19 21:28:21 +0100

    add simulation ds robot physique

---
+++ Robot phyique/simulation.py
@@ -0,0 +1,17 @@
+#import
+
+
+#code
+
+
+class Simulation:
+    def __init__(self,strategie):
+        self.strategie=strategie
+
+    def run(self):
+        cpt=0
+        while self.strategie.stop==False:
+            self.strategie.update()
+            cpt+=1
+        print("Arret")
+        
