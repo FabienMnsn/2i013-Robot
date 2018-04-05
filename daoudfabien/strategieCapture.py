33ca698d713b5ad7427287213ff08afd7ed5e167
Unknown <fabien.manson@free.fr>
2018-04-05 10:01:35 +0200

    image

--- daoudfabien/strategieCapture.py
+++ daoudfabien/strategieCapture.py
@@ -3,7 +3,7 @@ from robot2I013 import *
 #code
 robot = Robot2I013()
 img = robot.get_image()
-img.save("/users/Etu3/3670293/L2/S2/2I013/Projet/Robot7M/daoudfabien/Images/Image.jpg")
+img.save("Image.jpg")
 
 """
 class strategieCamera:
