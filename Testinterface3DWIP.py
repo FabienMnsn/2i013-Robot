70295cadabcbeec335eac418c43a0a4f016573b1
DaoudKAD <35796222+DaoudKAD@users.noreply.github.com>
2018-05-05 15:48:43 +0200

    Add files via upload

--- Testinterface3DWIP.py
+++ Testinterface3DWIP.py
@@ -21,27 +21,27 @@ main = Window(720, 480, "Arena", resizable=True)
 a = Arene(8000,800,8000,[],[]) # en changeant les 3 premieres valeurs on modifie la taille de l'arene
 a.generateur_arene()
 
-b = Balise(0,0,-2000,800)
+b = Balise(160,0,-80,500)
 
-r = Creation_Robot(0,0,0,-1)
+r = Creation_Robot(0,0,-0.4,1)
 
-c0 = Cube(2000,0,-1000,500,500,900)
-c1 = Cube(3500,0,2500,600,800,400)
+c0 = Cube(400,0,-1000,500,500,500)
+c1 = Cube(2000,0,2500,400,400,400)
 a.ajouter_cube(c0)
 a.ajouter_cube(c1)
 
 #setup de strategie et simulation
-strat70 = strategieToutDroit70(r)
+#strat70 = strategieToutDroit70(r)
 #simulation = Simulation(strat70)
 
 # setup des vue 3D
 main.addVueArene(a)
 main.addVueRobot(r)
 main.addVueBalise(b)
-main.addStrat(strat70)
+#main.addStrat(strat70)
 
 #setup position camera
-main.eye = (0,3000,3000)
+main.eye = (0,8000,8000)
 main.lookat = (0,100,0)
 main.up = (0,0,-1)
 
